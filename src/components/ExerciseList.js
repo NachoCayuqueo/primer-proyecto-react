78b469ceb5c4644a44937bc4f7ad1d09443a992d
Nacho Cayuqueo <cayuqueoNacho@gmail.com>
2021-02-08 22:00:38 -0300

    it was modified to function with arrow

--- src/components/ExerciseList.js
+++ src/components/ExerciseList.js
@@ -1,6 +1,24 @@
 import React from "react";
 import Card from "./Card";
 
+const ExerciseList = ({ exercises }) => (
+  <div>
+    {exercises.map((exercise) => {
+      return (
+        <Card
+          key={exercise.id}
+          title={exercise.title}
+          description={exercise.description}
+          img={exercise.img}
+          leftColor={exercise.leftColor}
+          rightColor={exercise.rightColor}
+        />
+      );
+    })}
+  </div>
+);
+
+/*
 function ExerciseList(props) {
   return (
     <div>
@@ -17,6 +35,6 @@ function ExerciseList(props) {
       })}
     </div>
   );
-}
+}*/
 
 export default ExerciseList;
--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,6 +1,16 @@
 import React from "react";
 import "./styles/Welcome.css";
 
+const Welcome = ({ username }) => (
+  <div className="container">
+    <div className="Fitness-User-Info">
+      <h1>Hello {username}!</h1>
+      <p>Let's workout to get someone gains!</p>
+    </div>
+  </div>
+);
+
+/*
 function Welcome(props) {
   return (
     <div className="container">
@@ -10,6 +20,6 @@ function Welcome(props) {
       </div>
     </div>
   );
-}
+}*/
 
 export default Welcome;
